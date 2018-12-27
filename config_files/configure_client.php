23990803b50172faff46ffb5d058d33fdf4d06a0
Nathan <35417308+Xc1d30us-Mercy@users.noreply.github.com>
2018-12-27 00:20:30 -0700

    modded config files for raspAP

---
+++ config_files/configure_client.php
@@ -0,0 +1,296 @@
+<?php
+
+/**
+*
+*
+*/
+function DisplayWPAConfig(){
+  $status = new StatusMessages();
+  $networks = array();
+
+  // Find currently configured networks
+  exec(' sudo cat ' . RASPI_WPA_SUPPLICANT_CONFIG, $known_return);
+
+  $network = null;
+  $ssid = null;
+
+  foreach($known_return as $line) {
+    if (preg_match('/network\s*=/', $line)) {
+      $network = array('visible' => false, 'configured' => true, 'connected' => false);
+    } elseif ($network !== null) {
+      if (preg_match('/^\s*}\s*$/', $line)) {
+        $networks[$ssid] = $network;
+        $network = null;
+        $ssid = null;
+      } elseif ($lineArr = preg_split('/\s*=\s*/', trim($line))) {
+        switch(strtolower($lineArr[0])) {
+          case 'ssid':
+            $ssid = trim($lineArr[1], '"');
+            break;
+          case 'psk':
+            if (array_key_exists('passphrase', $network)) {
+              break;
+            }
+          case '#psk':
+            $network['protocol'] = 'WPA';
+          case 'wep_key0': // Untested
+            $network['passphrase'] = trim($lineArr[1], '"');
+            break;
+          case 'key_mgmt':
+            if (! array_key_exists('passphrase', $network) && $lineArr[1] === 'NONE') {
+              $network['protocol'] = 'Open';
+            }
+            break;
+          case 'priority':
+            $network['priority'] = trim($lineArr[1], '"');
+            break;
+        }
+      }
+    }
+  }
+
+   if ( isset($_POST['connect']) ) {
+    $result = 0;
+    exec ( 'sudo wpa_cli -i ' . RASPI_WPA_CTRL_INTERFACE . ' select_network '.strval($_POST['connect'] ));
+  }
+  else if ( isset($_POST['client_settings']) && CSRFValidate() ) {
+    $tmp_networks = $networks;
+    if ($wpa_file = fopen('/tmp/wifidata', 'w')) {
+      fwrite($wpa_file, 'ctrl_interface=DIR=' . RASPI_WPA_CTRL_INTERFACE . ' GROUP=netdev' . PHP_EOL);
+      fwrite($wpa_file, 'update_config=1' . PHP_EOL);
+
+      foreach(array_keys($_POST) as $post) {
+        if (preg_match('/delete(\d+)/', $post, $post_match)) {
+          unset($tmp_networks[$_POST['ssid' . $post_match[1]]]);
+        } elseif (preg_match('/update(\d+)/', $post, $post_match)) {
+          // NB, at the moment, the value of protocol from the form may
+          // contain HTML line breaks
+          $tmp_networks[$_POST['ssid' . $post_match[1]]] = array(
+            'protocol' => ( $_POST['protocol' . $post_match[1]] === 'Open' ? 'Open' : 'WPA' ),
+            'passphrase' => $_POST['passphrase' . $post_match[1]],
+            'configured' => true
+          );
+          if (array_key_exists('priority' . $post_match[1], $_POST)) {
+            $tmp_networks[$_POST['ssid' . $post_match[1]]]['priority'] = $_POST['priority' . $post_match[1]];
+          }
+        }
+      }
+
+      $ok = true;
+      foreach($tmp_networks as $ssid => $network) {
+        if ($network['protocol'] === 'Open') {
+          fwrite($wpa_file, "network={".PHP_EOL);
+          fwrite($wpa_file, "\tssid=\"".$ssid."\"".PHP_EOL);
+          fwrite($wpa_file, "\tkey_mgmt=NONE".PHP_EOL);
+          if (array_key_exists('priority', $network)) {
+            fwrite($wpa_file, "\tpriority=".$network['priority'].PHP_EOL);
+          }
+          fwrite($wpa_file, "}".PHP_EOL);
+        } else {
+          if (strlen($network['passphrase']) >=8 && strlen($network['passphrase']) <= 63) {
+            unset($wpa_passphrase);
+            unset($line);
+            exec( 'wpa_passphrase '.escapeshellarg($ssid). ' ' . escapeshellarg($network['passphrase']),$wpa_passphrase );
+            foreach($wpa_passphrase as $line) {
+              if (preg_match('/^\s*}\s*$/', $line)) {
+                if (array_key_exists('priority', $network)) {
+                  fwrite($wpa_file, "\tpriority=".$network['priority'].PHP_EOL);
+                }
+                fwrite($wpa_file, $line.PHP_EOL);
+              } else {
+                fwrite($wpa_file, $line.PHP_EOL);
+              }
+            }
+          } else {
+            $status->addMessage('WPA passphrase must be between 8 and 63 characters', 'danger');
+            $ok = false;
+          }
+        }
+      }
+
+      if ($ok) {
+        system( 'sudo cp /tmp/wifidata ' . RASPI_WPA_SUPPLICANT_CONFIG, $returnval );
+        if( $returnval == 0 ) {
+          exec('sudo wpa_cli -i ' . RASPI_WIFI_CLIENT_INTERFACE . ' reconfigure', $reconfigure_out, $reconfigure_return );
+          if ($reconfigure_return == 0) {
+            $status->addMessage('Wifi settings updated successfully', 'success');
+	    system( 'sudo ifdown wlan0' );
+	    system( 'sudo ifup wlan0' );
+            $networks = $tmp_networks;
+          } else {
+            $status->addMessage('Wifi settings updated but cannot restart (cannot execute "wpa_cli reconfigure")', 'danger');
+          }
+        } else {
+          $status->addMessage('Wifi settings failed to be updated', 'danger');
+        }
+      }
+    } else {
+      $status->addMessage('Failed to update wifi settings', 'danger');
+    }
+  }
+
+  exec( 'sudo wpa_cli -i ' . RASPI_WIFI_CLIENT_INTERFACE . ' scan' );
+  sleep(3);
+  exec( 'sudo wpa_cli -i ' . RASPI_WIFI_CLIENT_INTERFACE . ' scan_results',$scan_return );
+
+  for( $shift = 0; $shift < 2; $shift++ ) {
+    array_shift($scan_return);
+  }
+
+  // display output
+  foreach( $scan_return as $network ) {
+    $arrNetwork = preg_split("/[\t]+/",$network);  // split result into array
+
+    // Save RSSI
+    if (array_key_exists(4, $arrNetwork)) {
+      $networks[$arrNetwork[4]]['RSSI'] = $arrNetwork[2];
+    }
+
+    // If network is saved
+    if (array_key_exists(4, $arrNetwork) && array_key_exists($arrNetwork[4], $networks)) {
+      $networks[$arrNetwork[4]]['visible'] = true;
+      $networks[$arrNetwork[4]]['channel'] = ConvertToChannel($arrNetwork[1]);
+      // TODO What if the security has changed?
+    } else {
+      $networks[$arrNetwork[4]] = array(
+        'configured' => false,
+        'protocol' => ConvertToSecurity($arrNetwork[3]),
+        'channel' => ConvertToChannel($arrNetwork[1]),
+        'passphrase' => '',
+        'visible' => true,
+        'connected' => false
+      );
+    }
+  }
+
+  exec( 'iwconfig ' . RASPI_WIFI_CLIENT_INTERFACE, $iwconfig_return );
+  foreach ($iwconfig_return as $line) {
+    if (preg_match( '/ESSID:\"([^"]+)\"/i',$line,$iwconfig_ssid )) {
+      $networks[$iwconfig_ssid[1]]['connected'] = true;
+    }
+  }
+?>
+
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="panel panel-primary"> 
+        <div class="panel-heading"><i class="fa fa-signal fa-fw"></i> <?php echo _("Configure client"); ?></div>
+        <!-- /.panel-heading -->
+        <div class="panel-body">
+          <p><?php $status->showMessages(); ?></p>
+          <h4><?php echo _("Client settings"); ?></h4>
+              <div class="btn-group btn-block">
+	          <a href=".?<?php echo htmlspecialchars($_SERVER['QUERY_STRING'], ENT_QUOTES); ?>" style="padding:10px;float: right;display: block;position: relative;margin-top: -55px;" class="col-md-2 btn btn-info" id="update"><?php echo _("Rescan"); ?></a>
+	        </div> 
+
+            <form method="POST" action="?page=wpa_conf" name="wpa_conf_form">
+              <?php CSRFToken() ?>
+              <input type="hidden" name="client_settings" ?>
+              <script>
+                function showPassword(index) {
+                    var x = document.getElementsByName("passphrase"+index)[0];
+                    if (x.type === "password") {
+                        x.type = "text";
+                    } else {
+                        x.type = "password";
+                    }
+                }
+              </script>
+
+              <?php $index = 0; ?>
+              <?php foreach ($networks as $ssid => $network) { ?>
+ 
+              <div class="col-md-6">
+                <div class="panel panel-default">
+                  <div class="panel-body">
+
+                  <input type="hidden" name="ssid<?php echo $index ?>" value="<?php echo htmlentities($ssid, ENT_QUOTES) ?>" />
+                  <h4><?php echo htmlspecialchars($ssid, ENT_QUOTES); ?></h4>
+
+                  <div class="row">
+                    <div class="col-xs-4 col-md-4">Status</div>
+                    <div class="col-xs-4 col-md-4">
+                      <?php if ($network['configured']) { ?>
+                        <i class="fa fa-check-circle fa-fw"></i>
+                      <?php } ?>
+                      <?php if ($network['connected']) { ?>
+                        <i class="fa fa-exchange fa-fw"></i>
+                      <?php } ?>
+                     </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-xs-4 col-md-4">Channel</div>
+                    <div class="col-xs-4 col-md-4">
+                      <?php if ($network['visible']) { ?>
+                          <?php echo htmlspecialchars($network['channel'], ENT_QUOTES) ?>
+                      <?php } else { ?>
+                          <span class="label label-warning"> X </span>
+                      <?php } ?>
+                    </div>
+                  </div>
+
+                  <div class="row">
+                    <div class="col-xs-4 col-md-4">RSSI</div>
+                    <div class="col-xs-6 col-md-6">
+                    <?php echo htmlspecialchars($network['RSSI'], ENT_QUOTES);
+                        echo "dB (";
+                        if($network['RSSI'] >= -50) { echo 100; }
+                        else if($network['RSSI'] <= -100) { echo 0;}
+                        else {echo  2*($network['RSSI'] + 100); }
+                        echo "%)";
+                    ?>
+                    </div>
+                  </div>
+
+                  <?php if (array_key_exists('priority', $network)) { ?>
+                      <input type="hidden" name="priority<?php echo $index ?>" value="<?php echo htmlspecialchars($network['priority'], ENT_QUOTES); ?>" />
+                  <?php } ?>
+                  <input type="hidden" name="protocol<?php echo $index ?>" value="<?php echo htmlspecialchars($network['protocol'], ENT_QUOTES); ?>" />
+
+                  <div class="row">
+                    <div class="col-xs-4 col-md-4">Security</div>
+                    <div class="col-xs-6 col-md-6"><?php echo $network['protocol'] ?></div>
+                  </div>
+
+                  <div class="form-group">
+                    <div class="input-group col-xs-12 col-md-12">
+                      <span class="input-group-addon" id="passphrase">Passphrase</span>
+                      <?php if ($network['protocol'] === 'Open') { ?> 
+                          <input type="hidden" name="passphrase<?php echo $index ?>" value="" />--- 
+                      <?php } else { ?>
+                          <input type="password" class="form-control" aria-describedby="passphrase" name="passphrase<?php echo $index ?>" value="<?php echo $network['passphrase'] ?>" onKeyUp="CheckPSK(this, 'update<?php echo $index?>')" >
+                          <span class="input-group-btn">
+                            <button class="btn btn-default" onclick="showPassword(<?php echo $index; ?>)" type="button">Show</button>
+                          </span>
+                      <?php } ?>
+                    </div>
+                  </div>
+
+                  <div class="btn-group btn-block ">
+                    <?php if ($network['configured']) { ?>
+                        <input type="submit" class="col-xs-4 col-md-4 btn btn-warning" value="<?php echo _("Update"); ?>" id="update<?php echo $index ?>" name="update<?php echo $index ?>"<?php echo ($network['protocol'] === 'Open' ? ' disabled' : '')?> />
+                        <button type="submit" class="col-xs-4 col-md-4 btn btn-info" value="<?php echo $index?>" ><?php echo _("Connect"); ?></button>
+                    <?php } else { ?>
+                        <input type="submit" class="col-xs-4 col-md-4 btn btn-info" value="<?php echo _("Add"); ?>" id="update<?php echo $index ?>" name="update<?php echo $index ?>" <?php echo ($network['protocol'] === 'Open' ? '' : ' disabled')?> />
+                    <?php } ?>
+                        <input type="submit" class="col-xs-4 col-md-4 btn btn-danger" value="<?php echo _("Delete"); ?>" name="delete<?php echo $index ?>"<?php echo ($network['configured'] ? '' : ' disabled')?> />
+                  </div><!-- /.btn-group -->
+
+                </div><!-- /.panel-body -->
+              </div><!-- /.panel-default -->
+            </div><!-- /.col-md-6 -->
+
+            <?php $index += 1; ?>
+            <?php } ?>
+
+          </form>
+        </div><!-- ./ Panel body -->
+	<div class="panel-footer"><?php echo _("<strong>Note:</strong> WEP access points appear as 'Open'. RaspAP does not currently support connecting to WEP"); ?></div>
+      </div><!-- /.panel-primary -->
+    </div><!-- /.col-lg-12 -->
+  </div><!-- /.row -->
+<?php
+}
+
+?>
---
+++ config_files/index.php
@@ -0,0 +1,243 @@
+<?php
+
+/**
+ * Raspbian WiFi Configuration Portal
+ *
+ * Enables use of simple web interface rather than SSH to control wifi and hostapd on the Raspberry Pi.
+ * Recommended distribution is Raspbian Server Edition. Specific instructions to install the supported software are
+ * in the README and original post by @SirLagz. For a quick run through, the packages required for the WebGUI are:
+ * lighttpd (I have version 1.4.31-2 installed via apt)
+ * php5-cgi (I have version 5.4.4-12 installed via apt)
+ * along with their supporting packages, php5 will also need to be enabled.
+ * 
+ * @author     Lawrence Yau <sirlagz@gmail.com>
+ * @author     Bill Zimmerman <billzimmerman@gmail.com>
+ * @license    GNU General Public License, version 3 (GPL-3.0)
+ * @version    1.3.1
+ * @link       https://github.com/billz/raspap-webgui
+ * @see        http://sirlagz.net/2013/02/08/raspap-webgui/
+ */
+
+session_start();
+
+include_once( 'includes/config.php' );
+include_once( RASPI_CONFIG.'/raspap.php' );
+include_once( 'includes/locale.php');
+include_once( 'includes/functions.php' );
+include_once( 'includes/dashboard.php' );
+include_once( 'includes/authenticate.php' );
+include_once( 'includes/admin.php' );
+include_once( 'includes/dhcp.php' );
+include_once( 'includes/hostapd.php' );
+include_once( 'includes/system.php' );
+include_once( 'includes/configure_client.php' );
+include_once( 'includes/networking.php' );
+include_once( 'includes/themes.php' );
+include_once( 'includes/data_usage.php' );
+
+$output = $return = 0;
+$page = $_GET['page'];
+
+if (empty($_SESSION['csrf_token'])) {
+    if (function_exists('mcrypt_create_iv')) {
+        $_SESSION['csrf_token'] = bin2hex(mcrypt_create_iv(32, MCRYPT_DEV_URANDOM));
+    } else {
+        $_SESSION['csrf_token'] = bin2hex(openssl_random_pseudo_bytes(32));
+    }
+}
+$csrf_token = $_SESSION['csrf_token'];
+
+if(!isset($_COOKIE['theme'])) {
+    $theme = "custom.css";
+} else {
+    $theme = $_COOKIE['theme'];
+}
+
+$theme_url = 'dist/css/'.htmlspecialchars($theme, ENT_QUOTES);
+
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title><?php echo _("Raspbian WiFi Configuration Portal"); ?></title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- MetisMenu CSS -->
+    <link href="vendor/metisMenu/metisMenu.min.css" rel="stylesheet">
+
+    <!-- Timeline CSS -->
+    <link href="dist/css/timeline.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="dist/css/sb-admin-2.min.css" rel="stylesheet">
+
+    <!-- Morris Charts CSS -->
+    <link href="vendor/morrisjs/morris.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+    <!-- Custom CSS -->
+    <link href="<?php echo $theme_url; ?>" title="main" rel="stylesheet">
+
+    <link rel="shortcut icon" type="image/png" href="../img/favicon.png">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+    <div id="wrapper">
+      <!-- Navigation -->
+      <nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+	  <a class="navbar-brand" href="index.php"><?php echo _("RaspAP Wifi Portal"); ?> v<?php echo RASPI_VERSION; ?></a>
+        </div>
+        <!-- /.navbar-header -->
+
+        <!-- Navigation -->
+        <div class="navbar-default sidebar" role="navigation">
+          <div class="sidebar-nav navbar-collapse">
+            <ul class="nav" id="side-menu">
+              <li>
+                <a href="index.php?page=wlan0_info"><i class="fa fa-dashboard fa-fw"></i> <?php echo _("Dashboard"); ?></a>
+              </li>
+              <li>
+                <a href="index.php?page=wpa_conf"><i class="fa fa-signal fa-fw"></i> <?php echo _("Configure WiFi client"); ?></a>
+              </li>
+              <?php if ( RASPI_TORPROXY_ENABLED ) : ?>
+              <li>
+                 <a href="index.php?page=torproxy_conf"><i class="fa fa-eye-slash fa-fw"></i> <?php echo _("Configure TOR proxy"); ?></a>
+              </li>
+              <?php endif; ?>
+              <?php if ( RASPI_CONFAUTH_ENABLED ) : ?>
+              <li>
+                <a href="index.php?page=auth_conf"><i class="fa fa-lock fa-fw"></i> <?php echo _("Configure Auth"); ?></a>
+              </li>
+              <?php endif; ?>
+              <?php if ( RASPI_CHANGETHEME_ENABLED ) : ?>
+              <li>
+                <a href="index.php?page=theme_conf"><i class="fa fa-wrench fa-fw"></i> <?php echo _("Change Theme"); ?></a>
+              </li>
+              <?php endif; ?>
+              <?php if ( RASPI_VNSTAT_ENABLED ) : ?>
+              <li>
+                <a href="index.php?page=data_use"><i class="fa fa-bar-chart fa-fw"></i> <?php echo _("Data usage"); ?></a>
+              </li>
+              <?php endif; ?>
+              <li>
+                <a href="index.php?page=system_info"><i class="fa fa-cube fa-fw"></i> <?php echo _("System"); ?></a>
+              </li>
+            </ul>
+          </div><!-- /.navbar-collapse -->
+        </div><!-- /.navbar-default -->
+      </nav>
+
+      <div id="page-wrapper">
+
+        <!-- Page Heading -->
+        <div class="row">
+          <div class="col-lg-12">
+            <h1 class="page-header">
+              <img class="logo" src="img/raspAP-logo.png" width="45" height="45">RaspAP
+            </h1>
+          </div>
+        </div><!-- /.row -->
+
+        <?php 
+$extraFooterScripts = array();
+        // handle page actions
+        switch( $page ) {
+          case "wlan0_info":
+            DisplayDashboard();
+            break;
+          case "dhcpd_conf":
+            DisplayDHCPConfig();
+            break;
+          case "wpa_conf":
+            DisplayWPAConfig();
+            break;
+          case "network_conf":
+            DisplayNetworkingConfig();
+            break;
+          case "hostapd_conf":
+            DisplayHostAPDConfig();
+            break;
+          case "openvpn_conf":
+            DisplayOpenVPNConfig();
+            break;
+          case "torproxy_conf":
+            DisplayTorProxyConfig();
+            break;
+          case "auth_conf":
+            DisplayAuthConfig($config['admin_user'], $config['admin_pass']);
+            break;
+          case "save_hostapd_conf":
+            SaveTORAndVPNConfig();
+            break;
+          case "theme_conf":
+            DisplayThemeConfig();
+            break;
+          case "data_use":
+            DisplayDataUsage($extraFooterScripts);
+            break;
+          case "system_info":
+            DisplaySystem();
+            break;
+          default:
+            DisplayDashboard();
+        }
+
+?>
+      </div><!-- /#page-wrapper --> 
+    </div><!-- /#wrapper -->
+
+    <!-- RaspAP JavaScript -->
+    <script src="dist/js/functions.js"></script>
+
+    <!-- jQuery -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Metis Menu Plugin JavaScript -->
+    <script src="vendor/metisMenu/metisMenu.min.js"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="dist/js/sb-admin-2.js"></script>
+
+    <!-- Custom RaspAP JS -->
+    <script src="js/custom.js"></script>
+
+<?php
+// Load non default JS/ECMAScript in footer.
+foreach ($extraFooterScripts as $script) {
+    echo '    <script type="text/javascript" src="' , $script['src'] , '"';
+    if ($script['defer']) {
+        echo ' defer="defer"';
+    }
+
+    // if ($script['async']) { echo ( echo ' defer="async"'; ), intrigity=, nonce=  etc. etc.
+    echo '></script>' , PHP_EOL;
+}
+
+?>
+  </body>
+</html>
